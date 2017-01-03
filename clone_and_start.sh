13a61a73f41a8e275d17c164e1b300677d1ed8c7
Philipp Henkel <henkel42@gmail.com>
2017-01-03 18:40:08 +0100

    Switch to base64 encoding of SSH key

--- clone_and_start.sh
+++ clone_and_start.sh
@@ -4,17 +4,17 @@ set -e
 GIT_BRANCH=${GIT_BRANCH:-master}
 
 echo '+++ Welcome to node-from-git'
-echo '+++ GIT_URL     ' $GIT_URL
-echo '+++ GIT_BRANCH  ' $GIT_BRANCH
-if [ ! -z "$GIT_SSH_KEY" ]
+echo '+++ GIT_URL            ' $GIT_URL
+echo '+++ GIT_BRANCH         ' $GIT_BRANCH
+if [ ! -z "$GIT_SSH_KEY_BASE64" ]
 then
-  echo '+++ GIT_SSH_KEY  provided'
-  echo "$GIT_SSH_KEY"
-  echo "$GIT_SSH_KEY" > git_ssh_key
+  echo '+++ GIT_SSH_KEY_BASE64  provided'
+  echo "$GIT_SSH_KEY_BASE64" > git_ssh_key.b64
+  base64 -d git_ssh_key.b64 > git_ssh_key
   chmod 600 git_ssh_key
   export GIT_SSH_COMMAND='ssh -i git_ssh_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 else
-  echo '+++ GIT_SSH_KEY  not set'
+  echo '+++ GIT_SSH_KEY_BASE64  not set'
 fi
 echo '+++'
 
