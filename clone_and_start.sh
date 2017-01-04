282a48c7ce9242bddc0037587efd3411bf285858
Philipp Henkel <henkel42@gmail.com>
2017-01-04 18:52:58 +0100

    Clean up if re-started

--- clone_and_start.sh
+++ clone_and_start.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 set -e
 
+echo '+++ Welcome to node-from-git'
+
+# clean-up
+if [ -f git_ssh_key ]
+then
+  rm git_ssh_key
+fi
+
+if [ -d node-app ]
+then
+  rm -rf node-app
+fi
+
 GIT_BRANCH=${GIT_BRANCH:-master}
 
-echo '+++ Welcome to node-from-git'
 echo '+++ GIT_URL            ' $GIT_URL
 echo '+++ GIT_BRANCH         ' $GIT_BRANCH
+
 if [ ! -z "$GIT_SSH_KEY_BASE64" ]
 then
   echo '+++ GIT_SSH_KEY_BASE64  provided'
   echo "$GIT_SSH_KEY_BASE64" > git_ssh_key.b64
   base64 -d git_ssh_key.b64 > git_ssh_key
+  rm git_ssh_key.b64
   chmod 600 git_ssh_key
   export GIT_SSH_COMMAND='ssh -i git_ssh_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 else
